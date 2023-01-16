93f56c041f0804b75530a2fbd42814e970d87693
yb <bini011219@gmail.com>
2023-01-16 17:30:33 +0900

    quiz 6-2

--- week2/day03_A.py
+++ week2/day03_A.py
@@ -1,4 +1,4 @@
-#5-4
+#5-4,5
 
 salutation='Mr'
 name='john'
@@ -24,4 +24,36 @@ letter='''Dear {0} {1},
 
 print(letter.format(salutation,name,product,verbed,room,animals,amount,percent,spokesman,job_title))
 
-#143p 2번, 3번
+#5-6,7,8
+
+# research0='''an English submarine 이름은 (Boaty McBoatface)로 지어졌다.
+# an Australian racehorse 이름은 (Horsey McHorseface)로 지어졌다.
+# an Swedish train 이름은 (Trainy McTrainface)로 지어졌다. '''
+
+print('''%sy Mc%sface
+%sy Mc%sface
+%sy Mc%sface''' % ('boat'.capitalize(),'boat'.capitalize(),'gourd'.capitalize(),'gourd'.capitalize(),'spitz'.capitalize(),'spitz'.capitalize()))
+
+print('''{0}y Mc{0}face
+{1}y Mc{1}face
+{2}y Mc{2}face''' .format('boat'.capitalize(),'gourd'.capitalize(),'spitz'.capitalize()))
+
+print(f'''{'boat'.capitalize()}y Mc{'boat'.capitalize()}face
+{'gourd'.capitalize()}y Mc{'gourd'.capitalize()}face
+{'spitz'.capitalize()}y Mc{'spitz'.capitalize()}face''')
+
+#6-2
+guess_me=7
+number=1
+
+while True:
+    if number>guess_me:
+        print('oops')
+        break
+    elif number<guess_me:
+        print('too low')
+    else:
+        print('found it!')
+        break
+    number+=1
+
