4ce6969cdaa52fe8ca31230d6db3e611a7d06caa
yb <bini011219@gmail.com>
2023-01-16 17:35:05 +0900

    6.3

--- week2/day03_A.py
+++ week2/day03_A.py
@@ -57,3 +57,16 @@ while True:
         break
     number+=1
 
+#6-3
+guess_me=5
+
+for number in range(10):
+    if number > guess_me:
+        print('oops')
+        break
+    elif number < guess_me:
+        print('too low')
+    else:
+        print('found it!')
+        break
+
